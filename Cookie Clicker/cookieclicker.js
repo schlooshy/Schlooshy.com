8c0a253f1500f9da72e2800c3cd11930c2db299d
schlooshy <archie@kirkleesclimbing.co.uk>
2026-03-11 20:08:44 +0000

    Updated the power up system.

--- Cookie Clicker/cookieclicker.js
+++ Cookie Clicker/cookieclicker.js
@@ -1,14 +1,23 @@
 let count = 0;
 let multiplier = 1;
 const errormessage = document.getElementById("error");
+const powerup = document.getElementById("powerup");
+const counter = document.getElementById("counter");
+const numberinput = document.getElementById("numberinput");
 
 function powerUp() {
     console.log(`Powered up`);
-    if (count >= 100) {
+    if (count >= 500) {
+        count -= 500;
+        multiplier = 5
+        counter.innerHTML = count;
+    }else if (count >= 100) {
         count -= 100;
         multiplier = 2;
-        document.getElementById("counter").innerHTML = count;
-    }else{
+        counter.innerHTML = count;
+        powerup.innerHTML = "Power up <br> 5 Clicks Per Click <br> 500 Clicks Required"
+    }
+    else{
         errormessage.innerHTML = "You don't have enough clicks!"
     }
 }
@@ -16,6 +25,11 @@ function powerUp() {
 function pressed() {
     count += multiplier
 
-    document.getElementById("counter").innerHTML = count;
+    counter.innerHTML = count;
     errormessage.innerHTML = "";
+}
+
+function submit(){
+    counter.innerHTML = count;
+    count = Number(numberinput.value);
 }
